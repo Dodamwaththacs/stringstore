5092bdadfa3606141f4f472978cf663643b71402
Chamika Sandun <110515504+Dodamwaththacs@users.noreply.github.com>
2024-02-29 13:05:06 +0530

    Post Mapping accept raw data

--- src/main/java/nova/stringstore/controller/MyContractController.java
+++ src/main/java/nova/stringstore/controller/MyContractController.java
@@ -13,12 +13,14 @@ public class MyContractController {
     private MyContractService myContractService;
 
     @PostMapping("/store")
-    public void storeString(@RequestBody String value, @RequestParam String privateKey) throws Exception {
+    public void storeString(@RequestBody String value) throws Exception {
+        String privateKey = "0x4e3e829be55d927b0db97bcca136521c7dae95fb94a1737273e4b2ad4e08292b";
         myContractService.storeString(value, privateKey);
     }
 
     @GetMapping("/get")
-    public String getString(@RequestBody String privateKey) throws Exception {
+    public String getString() throws Exception {
+        String privateKey = "0x4e3e829be55d927b0db97bcca136521c7dae95fb94a1737273e4b2ad4e08292b";
         return myContractService.getString(privateKey);
     }
 }
